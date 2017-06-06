92e0c6c5e26bd52f9f0d57c9b9340e7bbe19b391
Duncan Cameron <phplist@dcameron.me.uk>
2017-06-06 16:13:16 +0100

    Use the mailgun library

---
+++ README.md
@@ -0,0 +1,58 @@
+# Mailgun Plugin #
+
+## Description ##
+
+This plugin sends emails through Mailgun using their API.
+
+## Installation ##
+
+### Dependencies ###
+
+This plugin is for phplist 3.3.0 or later and requires php version 5.4 or later.
+
+It requires the Common Plugin to be installed. You must install, or upgrade to, the latest version. See <https://github.com/bramley/phplist-plugin-common>
+
+It also requires the php curl extension to be installed.
+
+### Set the plugin directory ###
+The default plugin directory is `plugins` within the admin directory.
+
+You can use a directory outside of the web root by changing the definition of `PLUGIN_ROOTDIR` in config.php.
+The benefit of this is that plugins will not be affected when you upgrade phplist.
+
+### Install through phplist ###
+The recommended way to install is through the Plugins page (menu Config > Manage Plugins) using the package
+URL `https://github.com/bramley/phplist-plugin-mailgun/archive/master.zip`.
+The installation should create
+
+* the file Mailgun.php
+* the directory Mailgun
+
+### Install manually ###
+If the automatic installation does not work then you can install manually.
+Download the plugin zip file from <https://github.com/bramley/phplist-plugin-mailgun/archive/master.zip>
+
+Expand the zip file, then copy the contents of the plugins directory to your phplist plugins directory.
+This should contain
+
+* the file Mailgun.php
+* the directory Mailgun
+
+## Usage ##
+
+For guidance on using the plugin see the plugin's page within the phplist documentation site <https://resources.phplist.com/plugin/mailgun>
+
+## Support ##
+
+Please raise any questions or problems in the user forum <https://discuss.phplist.org/>.
+
+## Donation ##
+
+This plugin is free but if you install and find it useful then a donation to support further development is greatly appreciated.
+
+[![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=W5GLX53WDM7T4)
+
+## Version history ##
+
+    version         Description
+    1.0.0+20170608  Initial release
---
+++ plugins/Mailgun.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Mailgun plugin for phplist.
+ *
+ * This file is a part of Mailgun Plugin.
+ *
+ * This plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @category  phplist
+ *
+ * @author    Duncan Cameron
+ * @copyright 2017 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ */
+
+/**
+ * Registers the plugin with phplist.
+ */
+if (!interface_exists('EmailSender')) {
+    return;
+}
+
+class Mailgun extends phplistPlugin implements EmailSender
+{
+    const VERSION_FILE = 'version.txt';
+
+    /** @var Mailgun connector instance */
+    private $connector;
+
+    /*
+     *  Inherited variables
+     */
+    public $name = 'Mailgun Plugin';
+    public $authors = 'Duncan Cameron';
+    public $description = 'Send emails through Mailgun';
+    public $documentationUrl = 'https://resources.phplist.com/plugin/mailgun';
+    public $settings = array(
+        'mailgun_api_key' => array(
+            'value' => '',
+            'description' => 'API key',
+            'type' => 'text',
+            'allowempty' => false,
+            'category' => 'Mailgun',
+        ),
+        'mailgun_domain' => array(
+            'value' => '',
+            'description' => 'Domain',
+            'type' => 'text',
+            'allowempty' => false,
+            'category' => 'Mailgun',
+        ),
+    );
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->coderoot = dirname(__FILE__) . '/' . 'Mailgun' . '/';
+        parent::__construct();
+        $this->version = (is_file($f = $this->coderoot . self::VERSION_FILE))
+            ? file_get_contents($f)
+            : '';
+    }
+
+    /**
+     * Provide the dependencies for enabling this plugin.
+     *
+     * @return array
+     */
+    public function dependencyCheck()
+    {
+        global $emailsenderplugin;
+
+        return array(
+            'PHP version 5.4.0 or greater' => version_compare(PHP_VERSION, '5.4') > 0,
+            'phpList version 3.3.0 or greater' => version_compare(getConfig('version'), '3.3') > 0,
+            'No other plugin to send emails can be enabled' => (
+                empty($emailsenderplugin)
+                || get_class($emailsenderplugin) == __CLASS__
+            ),
+            'curl extension installed' => extension_loaded('curl'),
+            'Common Plugin installed' => phpListPlugin::isEnabled('CommonPlugin'),
+        );
+    }
+
+    /**
+     * Implement the EmailSender interface to send an email using the Mailgun API.
+     *
+     * @param PHPlistMailer $phpmailer mailer instance
+     * @param string        $headers   the message http headers
+     * @param string        $body      the message body
+     *
+     * @return bool success/failure
+     */
+    public function send(PHPlistMailer $phpmailer, $headers, $body)
+    {
+        static $client = null;
+        static $domain;
+
+        if ($client === null) {
+            $client = \Mailgun\Mailgun::create(getConfig('mailgun_api_key'));
+            $domain = getConfig('mailgun_domain');
+        }
+        $to = $phpmailer->getToAddresses();
+        $parameters = [
+            'from' => "$phpmailer->FromName <$phpmailer->From>",
+            'to' => $to[0][0],
+            'subject' => $phpmailer->Subject,
+        ];
+        /*
+         * Add any attached files as either attachments or inline.
+         * Mailgun requires the cid of inline attachments to match the file name
+         */
+        $files = [
+            'attachment' => [],
+            'inline' => [],
+        ];
+        $inlineCid = [];
+        $inlineName = [];
+
+        foreach ($phpmailer->getAttachments() as $item) {
+            list($content, $filename, $name, $encoding, $type, $isString, $disposition, $cid) = $item;
+            $fileDetails = ['filename' => $name, 'fileContent' => $content];
+
+            if ($disposition == 'inline') {
+                $files['inline'][] = $fileDetails;
+                $inlineCid[] = "cid:$cid";
+                $inlineName[] = "cid:$name";
+            } else {
+                $files['attachment'][] = $fileDetails;
+            }
+        }
+        /*
+         * for an html message both Body and AltBody will be populated
+         * for a plain text message only Body will be populated
+         */
+        $isHtml = $phpmailer->AltBody != '';
+
+        if ($isHtml) {
+            $body = $phpmailer->Body;
+
+            if ($inlineCid) {
+                $body = str_replace($inlineCid, $inlineName, $body);
+            }
+            $parameters['html'] = $body;
+            $parameters['text'] = $phpmailer->AltBody;
+        } else {
+            $parameters['text'] = $phpmailer->Body;
+        }
+
+        foreach ($phpmailer->getCustomHeaders() as $item) {
+            $parameters['h:' . $item[0]] = $item[1];
+        }
+        $result = $client->sendMessage($domain, $parameters, $files);
+
+        if ($result->http_response_code == 200 && $result->http_response_body->message == 'Queued. Thank you.') {
+            return true;
+        }
+        logEvent('send failed: ' . print_r($result, true));
+
+        return false;
+    }
+}
